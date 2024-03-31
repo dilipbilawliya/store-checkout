78f8a4873d80ad26514d9c22ce20b2275fa646e4
dilip <dilip92bilawliya@gmail.com>
2024-03-31 19:52:38 +0530

    created seperate class for discount calculation to strictly follow single responsiblity

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,12 @@
 require 'json'
-require 'byebug'
+require_relative 'discount_calculator'
 
 class Checkout
-  attr_reader :rules, :cart
+  attr_reader :cart
 
   def initialize(rules_file)
     @rules = load_rules(rules_file)
+    @discount = DiscountCalculator.new(@rules)
     @cart = Hash.new(0)
   end
 
@@ -16,7 +17,7 @@ class Checkout
   def total
     total_price = 0.0
     @cart.each do |item, quantity|
-      total_price += calculate_item_price(item, quantity)
+      total_price += @discount.apply(item, quantity)
     end
     total_price.round(2)
   end
@@ -26,44 +27,4 @@ class Checkout
   def load_rules(rules_file)
     JSON.parse(File.read(rules_file))
   end
-
-  def calculate_item_price(item, quantity)
-    rules = @rules[item]
-    base_price = rules['price'] * quantity
-    return 0 unless @rules[item]
-
-    if rules['discount']&.any? { |d| d =~ /\d+-for-\d+/ }
-      buy_sets, remaining_quantity, buy_quantity, free_quantity = calculate_buy_sets_and_remaining_quantity(quantity, rules['discount'])
-      base_price = calculate_price_for_buy_sets(buy_sets, rules['price'], buy_quantity)
-      base_price += remaining_quantity * rules['price']
-      base_price = apply_bulk_discount(base_price, rules) if rules['discount'].include?('bulk') && quantity >= rules['bulk_quantity']
-    elsif rules['discount'] && rules['discount'].first == 'bulk' && quantity >= rules['bulk_quantity']
-      base_price = apply_bulk_discount(base_price, rules)
-    end
-    
-    base_price
-  end
-  
-  def calculate_buy_sets_and_remaining_quantity(quantity, discount)
-    promotion_discount = discount.find { |d| d =~ /\d+-for-\d+/ }
-    return [0, quantity] unless promotion_discount
-  
-    buy_quantity, free_quantity = promotion_discount.scan(/\d+/).map(&:to_i)
-    buy_quantity -= free_quantity
-    buy_sets = quantity / (buy_quantity + free_quantity)
-    remaining_quantity = quantity % (buy_quantity + free_quantity)
-    [buy_sets, remaining_quantity, buy_quantity, free_quantity]
-  end
-  
-  def calculate_price_for_buy_sets(buy_sets, price_per_set, buy_quantity)
-    buy_sets * (buy_quantity * price_per_set)
-  end
-  
-  def apply_bulk_discount(base_price, rules)
-    if base_price >= rules['bulk_quantity'] * rules['price']
-      rules['bulk_price'] * (base_price / rules['price'])
-    else
-      base_price
-    end
-  end  
 end
---
+++ lib/discount_calculator.rb
@@ -0,0 +1,48 @@
+class DiscountCalculator
+  def initialize(rules)
+    @rules = rules
+  end
+
+  def apply(item, quantity)
+    return 0 unless @rules[item]
+
+    rules = @rules[item]
+    base_price = rules['price'] * quantity
+
+    if rules['discount'] && rules['discount'].any? { |d| d =~ /\d+-for-\d+/ }
+      buy_sets, remaining_quantity, buy_quantity, free_quantity = calculate_buy_sets_and_remaining_quantity(quantity, rules['discount'])
+      base_price = calculate_price_for_buy_sets(buy_sets, rules['price'], buy_quantity)
+      base_price += remaining_quantity * rules['price']
+      base_price = apply_bulk_discount(base_price, rules) if rules['discount'].include?('bulk') && quantity >= rules['bulk_quantity']
+    elsif rules['discount'] && rules['discount'].first == 'bulk' && quantity >= rules['bulk_quantity']
+      base_price = apply_bulk_discount(base_price, rules)
+    end
+
+    base_price
+  end
+
+  private
+
+  def calculate_buy_sets_and_remaining_quantity(quantity, discount)
+    promotion_discount = discount.find { |d| d =~ /\d+-for-\d+/ }
+    return [0, quantity] unless promotion_discount
+
+    buy_quantity, free_quantity = promotion_discount.scan(/\d+/).map(&:to_i)
+    buy_quantity -= free_quantity
+    buy_sets = quantity / (buy_quantity + free_quantity)
+    remaining_quantity = quantity % (buy_quantity + free_quantity)
+    [buy_sets, remaining_quantity, buy_quantity, free_quantity]
+  end
+
+  def calculate_price_for_buy_sets(buy_sets, price_per_set, buy_quantity)
+    buy_sets * (buy_quantity * price_per_set)
+  end
+
+  def apply_bulk_discount(base_price, rules)
+    if base_price >= rules['bulk_quantity'] * rules['price']
+      rules['bulk_price'] * (base_price / rules['price'])
+    else
+      base_price
+    end
+  end
+end
