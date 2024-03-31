d0e960f5488711bd6c281e74077c6a293f1e15dc
dilip <dilip92bilawliya@gmail.com>
2024-03-31 19:43:59 +0530

    added method total and calculate_item_price to calculate discount and total amount of the items in the cart

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -13,9 +13,49 @@ class Checkout
     @cart[item] += 1
   end
 
+  def total
+    total_price = 0.0
+    @cart.each do |item, quantity|
+      total_price += calculate_item_price(item, quantity)
+    end
+    total_price.round(2)
+  end
+
   private
 
   def load_rules(rules_file)
     JSON.parse(File.read(rules_file))
+  end
+
+  def calculate_item_price(item, quantity)
+    base_price = 0
+    remaining_quantity = quantity
+    if @rules[item]['discount'].length == 2
+      promotion_discount = ''
+      @rules[item]['discount'].each do |discount|
+        promotion_discount = discount if discount =~ /\d-for-\d+/
+      end
+      buy_quantity = promotion_discount[0].to_i - promotion_discount[-1].to_i
+      free_quantity = promotion_discount[-1].to_i
+      
+      buy_sets = quantity / (buy_quantity + free_quantity)
+      base_price += buy_sets * (buy_quantity * @rules[item]['price'])
+      remaining_quantity -= buy_sets * (buy_quantity + free_quantity)
+      base_price += remaining_quantity * @rules[item]['price']
+      base_price = @rules[item]['bulk_price'] * (base_price/@rules[item]['price']) if (base_price/@rules[item]['price']) >= @rules[item]['bulk_quantity']
+
+    elsif @rules[item]['discount'].first == 'bulk' && quantity >= @rules[item]['bulk_quantity']
+      base_price = @rules[item]['bulk_price'] * quantity
+    elsif @rules[item]['discount'].first =~ /\d-for-\d+/
+      promotion_discount = @rules[item]['discount'].first
+
+      buy_quantity = promotion_discount[0].to_i - promotion_discount[-1].to_i
+      free_quantity = promotion_discount[-1].to_i
+      buy_sets = quantity / (buy_quantity + free_quantity)
+      base_price += buy_sets * (buy_quantity * @rules[item]['price'])
+      remaining_quantity -= buy_sets * (buy_quantity + free_quantity)
+      base_price += remaining_quantity * @rules[item]['price']
+    end
+    base_price
   end
 end
