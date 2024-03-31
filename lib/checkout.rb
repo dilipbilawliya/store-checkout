9924f1fa4ba56fdbf1dbf10c91b46703c86efc7f
dilip <dilip92bilawliya@gmail.com>
2024-03-31 19:38:34 +0530

    added json file to store the item data and loaded it into checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,13 +2,20 @@ require 'json'
 require 'byebug'
 
 class Checkout
-  attr_reader :cart
+  attr_reader :rules, :cart
 
-  def initialize
+  def initialize(rules_file)
+    @rules = load_rules(rules_file)
     @cart = Hash.new(0)
   end
 
   def scan(item)
     @cart[item] += 1
+  end
+
+  private
+
+  def load_rules(rules_file)
+    JSON.parse(File.read(rules_file))
   end
 end
