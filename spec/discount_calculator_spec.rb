4d258e6119a493bc0f6408cf90537cc5e0a8b9a2
dilip <dilip92bilawliya@gmail.com>
2024-03-31 20:38:40 +0530

    added the case for the custom discount and also multiple discount at same time

--- spec/discount_calculator_spec.rb
+++ spec/discount_calculator_spec.rb
@@ -39,5 +39,49 @@ RSpec.describe DiscountCalculator do
         expect(subject.apply('NONEXISTENT', 1)).to eq(0)
       end
     end
+    context 'when using custom rules' do
+      let(:rules) do
+        {
+          'VOUCHER' => { 'price' => 5.00, 'discount' => ['2-for-1'] },
+          'TSHIRT' => { 'price' => 20.00, 'discount' => ['bulk', '2-for-1'], 'bulk_quantity' => 3, 'bulk_price' => 19.00 },
+          'MUG' => { 'price' => 7.50, 'discount' => ['3-for-2'] }
+        }
+      end
+      let(:calculator) { DiscountCalculator.new(rules) }
+
+      context 'when no discount applies' do
+        it 'returns the base price for an item' do
+          expect(calculator.apply('MUG', 12)).to eq(60.00)
+        end
+      end
+
+      context 'when 2-for-1 discount applies' do
+        it 'applies the discount correctly' do
+          expect(calculator.apply('VOUCHER', 2)).to eq(5.00)
+          expect(calculator.apply('VOUCHER', 3)).to eq(10.00)
+        end
+      end
+
+      context 'when bulk discount applies' do
+        it 'applies the discount correctly' do
+          expect(calculator.apply('TSHIRT', 2)).to eq(20.00)
+          expect(calculator.apply('TSHIRT', 6)).to eq(57.00)
+        end
+      end
+
+      context 'when both discounts apply' do
+        it 'applies both discounts correctly' do
+          expect(calculator.apply('TSHIRT', 8)).to eq(76.00)
+        end
+      end
+
+      context 'when multiple items with different discounts are purchased' do
+        it 'applies the appropriate discounts for each item' do
+          expect(calculator.apply('VOUCHER', 2)).to eq(5.00)
+          expect(calculator.apply('TSHIRT', 3)).to eq(40.00)
+          expect(calculator.apply('MUG', 50)).to eq(255.00)
+        end
+      end
+    end
   end
 end
